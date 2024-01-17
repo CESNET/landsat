148084a6c4822d0cdb3696ed2b488ebb75a005aa
Jiří Matějka <jirimatejka@cesnet.cz>
2024-01-17 12:52:54 +0100

    Hello world ;-)

---
+++ config/config.py
@@ -0,0 +1,17 @@
+"""
+LOG LEVELS:
+
+CRITICAL = 50
+FATAL = CRITICAL
+ERROR = 40
+WARNING = 30
+WARN = WARNING
+INFO = 20
+DEBUG = 10
+NOTSET = 0
+"""
+
+log_directory = './log'
+log_name = 'landsat.log'
+log_level = 20
+log_logger = "logger_landsat"
---
+++ main.py
@@ -0,0 +1,32 @@
+import logging
+import re
+from logging.handlers import TimedRotatingFileHandler
+from pathlib import Path
+
+import config.config as config
+
+
+def setup_logging(current_path):
+    log_dir = current_path + '/' + config.log_directory
+    log_file = log_dir + '/' + config.log_name
+
+    Path(log_dir).mkdir(parents=True, exist_ok=True)
+
+    logger_landsat = logging.getLogger(config.log_logger)
+    logger_landsat.setLevel(config.log_level)
+
+    log_format = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+
+    rotating_info_handler = TimedRotatingFileHandler(log_file, when="midnight")
+    rotating_info_handler.setFormatter(log_format)
+    rotating_info_handler.setLevel(config.log_level)
+    rotating_info_handler.suffix = "%Y%m%d%H%M%S"
+    rotating_info_handler.extMatch = re.compile(r"^\d{14}$")
+    logger_landsat.addHandler(rotating_info_handler)
+
+
+if __name__ == '__main__':
+    root_dir = str(Path(__file__).parent.resolve())
+
+    setup_logging(root_dir)
+    logging.getLogger(config.log_logger).info("=== LANDSAT INITIALIZATION ===")
