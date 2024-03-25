8bc81caa62995f87f37aabce34b0263f7a07e675
Jiří Matějka <jirimatejka@cesnet.cz>
2024-03-25 13:24:12 +0100

    HTTP server replaced to Sanic. Ready for production.

--- http_server/main.py
+++ http_server/main.py
@@ -1,12 +1,18 @@
 # aws must be set: https://du.cesnet.cz/cs/navody/object_storage/awscli/start
 
-from pathlib import Path
-import logging
 import sys
-
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import re
 import os
+import logging
+from logging.handlers import TimedRotatingFileHandler
+from pathlib import Path
 
+from sanic import Sanic, response
+
+host_name = "0.0.0.0"  # Listen everywhere
+server_port = 8080
+
+log_logger = "HttpServerLogger"
 log_directory = './log'
 log_name = 'http-server.log'
 log_level = 20
@@ -23,50 +29,49 @@ DEBUG = 10
 NOTSET = 0
 """
 
-host_name = "0.0.0.0"  # Listen everywhere
-server_port = 8080
-
 
 def setup_logging(current_path):
-    log_dir = current_path + '/' + log_directory
-    log_file = log_dir + '/' + log_name
+    log_dir = os.path.join(current_path, log_directory)
+    log_file = os.path.join(str(log_dir), log_name)
 
-    os.makedirs(log_dir, exist_ok=True)
-    Path(log_file).touch()
+    Path(str(log_dir)).mkdir(parents=True, exist_ok=True)
 
-    logging.basicConfig(
-        filename=log_file,
-        encoding='utf-8',
-        level=log_level,
-        format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
-        force=True
-    )
-    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+    logger_http_server = logging.getLogger(log_logger)
+    logger_http_server.setLevel(log_level)
+
+    log_format = logging.Formatter("%(asctime)s [%(threadName)s] [%(levelname)s]  %(message)s")
+
+    rotating_info_handler = TimedRotatingFileHandler(log_file, when="midnight")
+    rotating_info_handler.setFormatter(log_format)
+    rotating_info_handler.setLevel(log_level)
+    rotating_info_handler.suffix = "%Y%m%d%H%M%S"
+    rotating_info_handler.extMatch = re.compile(r"^\d{14}$")
+    logger_http_server.addHandler(rotating_info_handler)
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setLevel(log_level)
+    stdout_handler.setFormatter(log_format)
+    logger_http_server.addHandler(stdout_handler)
+
+    return logger_http_server
 
 
-class Server(BaseHTTPRequestHandler):
-    def do_GET(self):
-        s3_url = "s3://landsat" + self.path  # Prepares path to S3 bucket using requested URL
+logger = setup_logging(str(Path(__file__).parent.resolve()))
 
-        aws_command = "aws s3 --endpoint-url https://s3.cl4.du.cesnet.cz presign " + s3_url
-        s3_download_url = os.popen(aws_command).read()  # Generate temporary link to download file from S3 bucket
+app = Sanic("landsat_http_server")
 
-        # Redirecting to temporary S3 download link
-        self.send_response(301)
-        self.send_header('Location', s3_download_url)
-        self.end_headers()
+
+@app.route("/<path:path>")
+async def redirect(request, path):
+    s3_url = f"s3://landsat/{path}"  # Prepares path to S3 bucket using requested URL
+
+    aws_command = f"aws s3 --endpoint-url https://s3.cl4.du.cesnet.cz presign {s3_url}"
+    s3_download_url = os.popen(aws_command).read().strip()  # Generate temporary link to download file from S3 bucket
+
+    logger.info(f"Got path: {path}, redirecting to: " + s3_download_url)
+
+    return response.redirect(s3_download_url)
 
 
 if __name__ == "__main__":
-    setup_logging(str(Path(__file__).parent.resolve()))
-
-    webServer = HTTPServer((host_name, server_port), Server)
-    print("Server started http://%s:%s" % (host_name, server_port))
-
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
+    app.run(host=host_name, port=server_port)
