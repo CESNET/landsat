a0adfcab83be8faa77c87af9012b406ffdcf384d
matejkaj <jirimatejka@cesnet.cz>
2025-03-18 10:22:51 +0100

    Updated time of new acquisition

--- downloader/main.py
+++ downloader/main.py
@@ -86,8 +86,8 @@ if __name__ == '__main__':
         while True:  # The downloading itself, repeat indefinitely
             next_run_at = datetime.datetime.combine(
                 datetime.datetime.now(datetime.UTC).date() + datetime.timedelta(days=1),
-                datetime.time(hour=9, minute=00)
-            )  # Next run tommorow at 9am UTC
+                datetime.time(hour=23, minute=00)
+            )  # Next run tomorrow at 23:00 UTC
 
             try:
                 landsat_downloader.run()
